b8a7b5848aef269c4bdf24b15b5c7fc15fb32697
MikeChang <s310143@hotmail.com>
2022-12-14 00:37:12 +0800

    37

--- src/components/Canvas/CanvasObj/index.tsx
+++ src/components/Canvas/CanvasObj/index.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useDraggable, DraggableObj } from "../hooks/useDraggable";
+import "./index.scss";
+type prop = {
+  ele: DraggableObj;
+  reset_active: Function;
+};
+function CanvasObj(props: prop) {
+  const draggable = useDraggable(props.ele);
 
-function index() {
-  return <div></div>;
+  return (
+    <div>
+      <div ref={draggable.ref} style={draggable.style}>
+        <p>drag me</p>
+        <p>
+          {`x: ${draggable.position.x.toFixed(0)}`}
+          {`y: ${draggable.position.y.toFixed(0)}`}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default index;
+export default CanvasObj;
